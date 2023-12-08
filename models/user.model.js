cfb0334daba73dc4535b7b051b88ef3fee573fa6
mateoghidini1998 <mateghidini123@gmail.com>
2023-12-08 18:34:28 -0300

    Create User model

---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
