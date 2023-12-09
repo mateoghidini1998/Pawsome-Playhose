d79d159897383ca8da633b53e83c11d11c8ed36e
mateoghidini1998 <mateghidini123@gmail.com>
2023-12-08 23:20:51 -0300

    Create method to get all user and a user by id

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,3 +46,32 @@ router.post('/', [
  });
  
 module.exports = router;
+
+//@route    GET api/users
+//@desc     Get all users
+//@access   Public
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Server error');
+    }
+
+});
+
+//@route    GET api/users/:id
+//@desc     Get user by id
+//@access   Public
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        res.json(user);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Server error');
+    }
+});
