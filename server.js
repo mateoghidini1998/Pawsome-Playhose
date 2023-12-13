77f96776f0ce8b662e967ac515a789e9f9f92bbc
mateoghidini1998 <mateghidini123@gmail.com>
2023-12-13 19:40:42 -0300

    Add auth routes

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.get('/', (req, res) => res.send('API Running'));
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 
 
