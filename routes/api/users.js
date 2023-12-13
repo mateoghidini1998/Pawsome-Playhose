a29f9bca120f12ca1ef54b225e3f410a57bb4835
mateoghidini1998 <mateghidini123@gmail.com>
2023-12-13 16:09:42 -0300

    Add functionality to hash users password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/user.model.js');
 const { check , validationResult } = require('express-validator/');
 
@@ -34,8 +37,33 @@ router.post('/', [
             return res.status(400).json({ errors: [{ msg: 'User already exists'}]});
         }
  
+        //Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        hashedPassword = await bcrypt.hash(password, salt);
+
+
         //Create user
-        user = await User.create({ name, lastname, email, password });
+        user = await User.create({ name, lastname, email, password:hashedPassword });
+
+        //Return jsonwebtoken -> this for users to be logged in right after registration
+
+        const payload = {
+            user:{
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn: 360000}, //change to 3600 for production
+            (err, token)=>{
+                if(err) throw err;
+                res.json({ token });
+            }
+            )
+
+
         return res.status(201).json({ user });
  
     } catch (error) {
