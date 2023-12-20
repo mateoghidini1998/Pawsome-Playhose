e014b1340facd17c696babe594ad73d80c946538
mateoghidini1998 <mateghidini123@gmail.com>
2023-12-19 22:38:41 -0300

    Create starter file for pets routes

---
+++ routes/api/pets.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const Pet = require('../../models/pet.model.js');
+
+//@route    POST api/pets
+//@desc     Register a pet
+//@access   Private
+
+router.post('/', (req, res) => {
+    
+})
