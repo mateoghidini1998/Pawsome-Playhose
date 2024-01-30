8467ca25df1bbfc3211f1f8bb9ebeb4c06076297
mateoghidini1998 <mateghidini123@gmail.com>
2024-01-30 20:15:55 -0300

    Clean code and call methods from model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,6 +8,53 @@ const jwt = require('jsonwebtoken');
 const { check , validationResult } = require('express-validator/');
 
 
+//@route    POST api/auth/register
+//@desc     Register a user
+//@access   Public
+
+router.post('/register', [
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('lastname', 'Lastname is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Plese include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+ ], 
+ async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors:errors.array()}); //400 is for bad requests
+    }
+ 
+    const { name, lastname, email, password } = req.body;
+ 
+    try {
+        
+        //Check if user exists
+        let user = await User.findOne({ where: { email } });
+ 
+        if(user){
+            return res.status(400).json({ errors: [{ msg: 'User already exists'}]});
+        }
+ 
+        //Create user
+        user = await User.create({ name, lastname, email, password });
+
+        //Return jsonwebtoken -> this for users to be logged in right after registration
+
+        sendTokenResponse(user, 200, res);
+       
+ 
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Server error');
+    }
+ 
+ });
+
+
 //@route   POST api/auth
 //@desc    Authenticate user & get token
 //@access  public
@@ -25,40 +72,31 @@ async (req, res)=> {
 
 
     const { email, password } = req.body;
+    console.log('Received Password:', password);
 
     try{
-        //See if user exists
 
-        let user = await User.findOne({ where: { email: email } });
+        // Validate emil & password
+        if (!email || !password) {
+            return next(new ErrorResponse('Please provide an email and password', 400));
+        }
+
+        //See if user exists
+        let user = await User.findOne({ where: { email: email }, attributes: ['email', 'password'] });
 
         if(!user){
             return res.status(400).json({ errors: [{ msg:'Invalid credentials' }] });
         }
 
         //Compare the input password, plane text, to the encrypted password.
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await user.matchPassword(password);
         
         if(!isMatch){
-            return res.status(400).json({ errors: [{ msg:'Invalid credentials' }] });
+            return res.status(401).json({ errors: [{ msg:'Invalid credentials' }] });
         }
     
         //Return jsonwebtoken -> this for users to be logged in right after registration
-
-        const payload = {
-            user:{
-                id: user.id
-            }
-        }
-
-        jwt.sign(
-            payload,
-            config.get('jwtSecret'),
-            {expiresIn: 360000}, //change to 3600 for production
-            (err, token)=>{
-                if(err) throw err;
-                res.json({ token });
-            }
-            )
+        sendTokenResponse(user, 200, res);
 
     }catch(err){
         console.error(err.message);
@@ -90,3 +128,39 @@ router.get('/me', auth, async (req, res)=>{
         res.status(500).send('Server Error');
     }
 })
+
+
+//@route   GET api/auth/logout
+//@desc    Logout user
+//@access  Private
+
+router.get('/logout', auth, async (req, res)=>{
+    
+})
+
+
+//Get Token from model, create a cookie and send response
+
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+  
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true
+    };
+  
+    if (process.env.NODE_ENV === 'production') {
+      options.secure = true;
+    }
+  
+    res
+      .status(statusCode)
+      .cookie('token', token, options)
+      .json({
+        success: true,
+        token
+      });
+  };
