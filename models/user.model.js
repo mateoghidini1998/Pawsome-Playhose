d8c6c7f12dfcd703ebc63dfdb1b76b3e129b0052
mateoghidini1998 <mateghidini123@gmail.com>
2024-01-30 20:15:18 -0300

    Add methods to encrypt and verify password

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = sequelize.define('User', {
     id: {
@@ -25,5 +27,40 @@ const User = sequelize.define('User', {
         allowNull: false
     }
 });
+
+
+User.addHook('beforeCreate', async (user, options) => {
+    if (user.password) {
+        console.log('Original Password:', user.password);
+        const salt = await bcrypt.genSalt(10);
+        console.log('Generated Salt:', salt);
+        user.password = await bcrypt.hash(user.password, salt);
+        console.log('Hashed Password:', user.password);
+    }
+});
+
+
+   
+User.prototype.getSignedJwtToken = function() {
+    return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+       expiresIn: process.env.JWT_EXPIRE
+    });
+};
+   
+User.prototype.matchPassword = function(enteredPassword) {
+    console.log('Entered Password:', enteredPassword);
+    console.log('Stored Password:', this.password);
+    
+    try {
+        const isMatch = bcrypt.compareSync(enteredPassword.trim(), this.password.trim());
+        console.log('Password Match Result:', isMatch);
+        return isMatch;
+    } catch (error) {
+        console.error('Error in matchPassword:', error);
+        return false;
+    }
+};
+
+
 
 module.exports = User;
