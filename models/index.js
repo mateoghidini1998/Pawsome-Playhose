c1d7a6b91a1ecffc4037687e591e3c8358afb951
mateoghidini1998 <mateghidini123@gmail.com>
2024-02-02 16:58:31 -0300

    Add Profile/User relationship

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 const User = require('./User.model');
 const UserType = require('./UserType.model');
+const Profile = require('./Profile.model');
 
 UserType.belongsToMany(User, { through: 'User_Types' });
 User.belongsToMany(UserType, { through: 'User_Types' });
+User.hasOne(Profile, { foreignKey: 'user_id' });
+Profile.belongsTo(User, { foreignKey: 'user_id' });
