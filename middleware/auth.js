83b927c23e3d7f5d38302a0963fe35d4373f4b08
mateoghidini1998 <mateghidini123@gmail.com>
2024-02-02 13:42:58 -0300

    Modify auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,38 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User.model');
 
-module.exports = function(req, res, next){
+//Protect Routes 
+exports.protect = asyncHandler(async (req, res, next) => {
+    let token;
 
-    //Get token from header
-    const token = req.header('x-auth-token');
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.token) {
+        token = req.cookies.token;
+    }
 
-    //Check if no token
-
+    //Make sure token exists
     if(!token){
-        return res.status(401).json({ msg: 'No token, authorization denied' }); //Not authorized
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 
-    //Verify token
-    try{
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
-        req.user = decoded.user;
-        next();
+    try {
+        /* console.log(token);*/
         
-    }catch(err){
-        res.status(401).json({ msg: 'Token is not valid' });
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findByPk(decoded.id);
+        console.log(user);
+        if (!user) {
+            return next(new ErrorResponse('User not found', 404));
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
-}
+    
+    
+});
+
